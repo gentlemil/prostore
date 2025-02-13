31176c7e827d1651879a214334e27ab106247251
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2025-02-13 12:13:28 +0100

    feat: add loading and not-found components (basic view)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,6 @@
+// testing loading spinner (make HomePage async and run await delay(2000))
+// const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
 const HomePage = () => {
   return <>Prostore</>
 }
