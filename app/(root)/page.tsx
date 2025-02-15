3767eec067c4f67d7d497124bd4831feef1d0bc3
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2025-02-15 21:43:27 +0100

    Feat: creat product list, card and price components and add simple mock data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
 // testing loading spinner (make HomePage async and run await delay(2000))
 // const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
+import sampleData from '@/db/sample-data'
+import ProductList from '@/components/shared/product/product-list'
+
 const HomePage = () => {
-  return <>Prostore</>
+  console.log(sampleData)
+
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  )
 }
 
 export default HomePage
